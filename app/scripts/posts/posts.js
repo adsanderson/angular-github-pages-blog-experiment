f89d1a7fa7179d5aadb767b285c989eaf2a5a57b
Adam Sanderson <lazydayed@gmail.com>
2013-11-28 23:43:54 +0000

    workout previous and next posts

--- app/scripts/posts/posts.js
+++ app/scripts/posts/posts.js
@@ -1,13 +1,38 @@
 
 angular.module('adsPosts', [])
-  .controller('postCtrl', function ($scope, $routeParams, metaData, _) {
+  .controller('postCtrl', function ($scope, $routeParams, $filter, metadata, _) {
 	'use strict';
     $scope.postFile = 'compiledPost/app/posts/' + $routeParams.postFile + '.html';
     // $scope.test = metaData.metaData;
 
-	var currentPostIndex = _.findIndex(metaData.metaData, {compiledPost:$routeParams.postFile});
+    $scope.test = metadata.getMetaData();
 
-	$scope.prevPost = metaData.metaData[currentPostIndex - 1];
-	$scope.nextPost = metaData.metaData[currentPostIndex + 1];
+	$scope.currentPostIndex = _.findIndex( $scope.test.meta, {compiledPost:$routeParams.postFile});
 
+	// console.log('cpi = ' + currentPostIndex);
+
+
+
+	//$scope.prevPost = metadata.getMetaData().meta[_.findIndex(metadata.getMetaData().meta, {compiledPost:$routeParams.postFile}) - 1];
+	//$scope.nextPost = metadata.getMetaData().data[currentPostIndex + 1];
+
+	// $scope.prevPost = $filter('prevNext')($scope.test, false);
+	// $scope.nextPost = $filter('prevNext')($scope.test, true);
+
+  }).filter('prevNext', function ($routeParams, _) {
+	'use strict';
+	return function(input, next) {
+		var out = false;
+		var currentPostIndex = _.findIndex(input.meta, {compiledPost:$routeParams.postFile});
+
+		if (currentPostIndex !== -1) {
+			if (next && currentPostIndex < input.meta.length ) {
+				out = input.meta[currentPostIndex + 1].compiledPost;
+			} else if (currentPostIndex > 0){
+				out = input.meta[currentPostIndex - 1].compiledPost;
+			}
+		}
+
+		return out;
+	};
   });
