8f0f1ded3c612f7898bc5a9d8ed1cd24cd0e68d1
Adam Sanderson <lazydayed@gmail.com>
2013-11-29 16:53:31 +0000

    add data filters

--- app/scripts/posts/posts.js
+++ app/scripts/posts/posts.js
@@ -3,22 +3,7 @@ angular.module('adsPosts', [])
   .controller('postCtrl', function ($scope, $routeParams, $filter, metadata, _) {
 	'use strict';
     $scope.postFile = 'compiledPost/app/posts/' + $routeParams.postFile + '.html';
-    // $scope.test = metaData.metaData;
-
     $scope.test = metadata.getMetaData();
-
-	$scope.currentPostIndex = _.findIndex( $scope.test.meta, {compiledPost:$routeParams.postFile});
-
-	// console.log('cpi = ' + currentPostIndex);
-
-
-
-	//$scope.prevPost = metadata.getMetaData().meta[_.findIndex(metadata.getMetaData().meta, {compiledPost:$routeParams.postFile}) - 1];
-	//$scope.nextPost = metadata.getMetaData().data[currentPostIndex + 1];
-
-	// $scope.prevPost = $filter('prevNext')($scope.test, false);
-	// $scope.nextPost = $filter('prevNext')($scope.test, true);
-
   }).filter('prevNext', function ($routeParams, _) {
 	'use strict';
 	return function(input, next) {
@@ -35,4 +20,16 @@ angular.module('adsPosts', [])
 
 		return out;
 	};
+  }).filter('title', function ($routeParams, _) {
+    'use strict';
+    return function(input) {
+        var out = false;
+        var currentPost = _.find(input.meta, {compiledPost:$routeParams.postFile});
+
+        if (typeof currentPost !== 'undefined') {
+           out = currentPost.title
+        }
+
+        return out;
+    };
   });
